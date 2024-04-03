4e3a6e979baf0500a94554c2172919f84cc76f96
UlysselaGlisse <ulysse38250@gmail.com>
2024-04-03 22:02:00 +0200

    ajout mdiff

--- Codes/snapwarns.sql
+++ Codes/snapwarns.sql
@@ -1,11 +1,5 @@
-CREATE or REPLACE FUNCTION erreurs() RETURNS TRIGGER AS $$
+/* You have to copy and paste this statement after you have click on execute sql inside Qgis */
 
-BEGIN
-
-
-DROP TABLE Erreurs;
-
-CREATE TABLE Erreurs as 
 
 /****************   Snap warn. Check if all the following segments are well snap *************************
 *****************       and check if two followings segments are the same id      ************************/
@@ -17,7 +11,7 @@ WITH a AS (SELECT compositions.id as compoid, ordinality AS num, segid, st_colle
 
 SELECT b.segid, null as num, b.geom 
 FROM a join a b on a.compoid = b.compoid and a.num+1 = b.num
-WHERE NOT st_intersects(a.geom, b.geom) or a.segid = b.segid
+WHERE st_distance(a.geom,b.geom) > 0.0000001 or a.segid = b.segid
 GROUP BY b.segid, b.geom
 
 UNION 
@@ -32,22 +26,6 @@ UNION
 
 /***************** Add random errors, just to keep the layer after Qgsi reload *************/
 
-SELECT id , null as num, null as geom
+SELECT id , null as num, geom
 FROM compositions
 WHERE id = 530
-
-RETURN NEW;
-END;
-$$ LANGUAGE plpgsql;
-
-CREATE or REPLACE TRIGGER erreurs_compo
-AFTER UPDATE of segments
-ON compositions
-FOR EACH ROW
-EXECUTE FUNCTION erreurs();
-
-CREATE or REPLACE TRIGGER erreurs_snap
-AFTER UPDATE 
-ON segments
-FOR EACH ROW
-EXECUTE FUNCTION erreurs();
