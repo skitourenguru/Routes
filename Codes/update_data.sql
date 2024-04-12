e0032039627faa4d7bfd8b5b2720398686a14d67
UlysselaGlisse <ulysse38250@gmail.com>
2024-04-12 15:40:45 +0200

    Ajout code pour renouveller ses données en cas de problème

--- Codes/update_data.sql
+++ Codes/update_data.sql
@@ -1,8 +1,6 @@
 ************************************************************************
-************************** Renew data in Database **********************
+************************** Update data in Database **********************
 ************************************************************************
-
-
 	
 -- Truncate the existing tables
 
@@ -16,19 +14,10 @@ DISABLE TRIGGER ALL;
 ALTER TABLE segments
 DISABLE TRIGGER ALL;
 
--- Change data type 
-
-
-ALTER TABLE compositions
-    ALTER segments TYPE TEXT USING array_to_string(segments, ',');
-
-
-
-
+------------------------------------------------------------------------------------
 Adapt and copy this line in your shell :
 ogr2ogr -f PostgreSQL "PG:user=postgres password=postgres dbname=test" /home/ulysse/Dropbox/skitourenguru/Ski-Routes/France-Alpes/France_Alpes_Compositions.geojson && ogr2ogr -f PostgreSQL "PG:user=postgres password=postgres dbname=test" /home/ulysse/Dropbox/skitourenguru/Ski-Routes/France-Alpes/France_Alpes_Segments.geojson	
-
-
+---------------------------------------------------------------------------------
 
 	
 -- Enable again the triggers
@@ -38,11 +27,6 @@ ENABLE TRIGGER ALL;
 
 ALTER TABLE segments
 ENABLE TRIGGER ALL;
-
--- Change data type again
-
-ALTER TABLE compositions
-	ALTER segments TYPE INTEGER[] USING string_to_array(segments, ',')::int[];
 
 -- Insert a first time geometry into compositions layer.
 
