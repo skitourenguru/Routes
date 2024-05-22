d5d6a81940abda8a0d2f768d42d389ca1258020c
UlysselaGlisse <ulysse38250@gmail.com>
2024-05-22 14:46:58 +0200

    e

--- Codes/Trigger_functions/split_and_merge.sql
+++ Codes/Trigger_functions/split_and_merge.sql
@@ -7,8 +7,8 @@ IF EXISTS (SELECT id FROM segments GROUP BY id HAVING count(id)>1) THEN
 
     WITH 
         doublons AS (
-            SELECT id 
-            FROM segments 
+            SELECT 	id 
+            FROM 	segments 
             GROUP BY id 
             HAVING count(id)>1
         ),
@@ -20,28 +20,29 @@ IF EXISTS (SELECT id FROM segments GROUP BY id HAVING count(id)>1) THEN
                 startpoint AS (
                     SELECT segments.id AS id,
                            st_value(rast, st_startpoint(geom), true) AS altitude
-                    FROM segments
-                    JOIN mnt ON st_startpoint(geom) && mnt.rast
+                    FROM	segments
+                    JOIN 	mnt ON st_startpoint(geom) && mnt.rast
                 ),
                 endpoint AS (
-                    SELECT segments.id AS id,
-                           st_value(rast, st_endpoint(geom), true) AS altitude
-                    FROM segments
-                    JOIN mnt ON st_endpoint(geom) && mnt.rast
+                    SELECT	segments.id AS id,
+                           	st_value(rast, st_endpoint(geom), true) AS altitude
+                    FROM 	segments
+                    JOIN 	mnt ON st_endpoint(geom) && mnt.rast
                 ),
                 segments_uniques AS (
-                    SELECT segid,
-                           id 
-                    FROM compositions,
-                         unnest(segments) AS segid 
-                    WHERE cardinality(segments) = 1
+                    SELECT 	segid,
+                           	id 
+                    FROM 	compositions,
+                         	unnest(segments) AS segid 
+                    WHERE 	cardinality(segments) = 1
                 ),
         segment_unique_inverse AS (
-            SELECT segments_uniques.id AS compoid
-            FROM startpoint 
-            JOIN endpoint ON startpoint.id = endpoint.id
-            JOIN segments_uniques ON segments_uniques.segid = endpoint.id
-            WHERE startpoint.altitude > endpoint.altitude
+			SELECT 	segments_uniques.id AS compoid
+			FROM 	startpoint 
+			JOIN 	endpoint ON startpoint.id = endpoint.id
+			JOIN 	segments_uniques ON segments_uniques.segid = endpoint.id
+			JOIN 	doublons on doublons.id = endpoint.id
+			WHERE 	startpoint.altitude > endpoint.altitude
         ),
 
     /****************************************************************************************************************
@@ -49,19 +50,19 @@ IF EXISTS (SELECT id FROM segments GROUP BY id HAVING count(id)>1) THEN
     *****************************************************************************************************************/
     
                 b AS (
-                    SELECT compositions.id AS compoid,
-                           ORDINALITY AS num,
-                           segid,
-                           segments.geom
-                    FROM compositions,
-                         UNNEST(segments) WITH ordinality AS segid
-                    JOIN segments ON segments.id = segid
+                    SELECT 	compositions.id AS compoid,
+						   	ORDINALITY AS num,
+						  	segid,
+						  	segments.geom
+                    FROM 	compositions,
+                         	UNNEST(segments) WITH ordinality AS segid
+                    JOIN 	segments ON segments.id = segid
                 ),
         premier_milieu_inverse AS (
-                SELECT b.compoid
-                FROM b JOIN b c ON b.compoid = c.compoid AND b.num+1 = c.num 
-                JOIN doublons ON doublons.id = b.segid
-                WHERE st_startpoint(b.geom) && (c.geom)
+                SELECT 	b.compoid
+                FROM 	b JOIN b c ON b.compoid = c.compoid AND b.num+1 = c.num 
+                JOIN 	doublons ON doublons.id = b.segid
+                WHERE 	st_startpoint(b.geom) && (c.geom)
         ),        
         
     
@@ -70,51 +71,52 @@ IF EXISTS (SELECT id FROM segments GROUP BY id HAVING count(id)>1) THEN
     *****************************************************************************************************************/
     
                 avant_dernier AS (
-                    SELECT segments[array_length(segments, 1) -1] AS segid,
-                           compositions.id AS compoid,
-                           segments.geom
-                    FROM compositions
-                    JOIN segments ON segments.id = segments[array_length(segments, 1) - 1]
+                    SELECT 	segments[array_length(segments, 1) -1] AS segid,
+                           	compositions.id AS compoid,
+                          	segments.geom
+                    FROM 	compositions
+                    JOIN 	segments ON segments.id = segments[array_length(segments, 1) - 1]
                 ),
             
                 dernier AS (			
-                    SELECT segments[array_length(segments, 1)] AS segid,
-                           compositions.id AS compoid,
-                           segments.geom 
-                    FROM compositions
-                    JOIN segments ON segments.id = segments[array_length(segments, 1)]
+					SELECT 	segments[array_length(segments, 1)] AS segid,
+							compositions.id AS compoid,
+							segments.geom 
+					FROM 	compositions
+					JOIN 	segments ON segments.id = segments[array_length(segments, 1)]
+					JOIN 	doublons ON doublons.id = segments[array_length(segments, 1)]
                 ),
         dernier_inverse AS (
-            SELECT dernier.compoid
-            FROM dernier
-            JOIN avant_dernier ON avant_dernier.compoid = dernier.compoid
-            WHERE st_endpoint(dernier.geom) && avant_dernier.geom
+            SELECT 	dernier.compoid
+            FROM 	dernier
+            JOIN 	avant_dernier ON avant_dernier.compoid = dernier.compoid
+            WHERE 	st_endpoint(dernier.geom) && avant_dernier.geom
         )
 
 
-    UPDATE compositions
-    SET segments = CASE
-    WHEN   compositions.id IN (SELECT segment_unique_inverse.compoid FROM segment_unique_inverse)
-        OR compositions.id IN (SELECT premier_milieu_inverse.compoid FROM premier_milieu_inverse)
-        OR compositions.id IN (SELECT dernier_inverse.compoid FROM dernier_inverse)
+    UPDATE 	compositions
+    SET 	segments = CASE
+    WHEN   	compositions.id IN (SELECT segment_unique_inverse.compoid FROM segment_unique_inverse)
+        OR 	compositions.id IN (SELECT premier_milieu_inverse.compoid FROM premier_milieu_inverse)
+        OR 	compositions.id IN (SELECT dernier_inverse.compoid FROM dernier_inverse)
     THEN
-        segments[:array_position(segments,doublons.id)-1] 
-        ||(SELECT max(id)+1 FROM public.segments)::int|| 
-        segments[array_position(segments, doublons.id):]
+        	segments[:array_position(segments,doublons.id)-1] 
+        	||(SELECT max(id)+1 FROM segments)::int|| 
+       	 	segments[array_position(segments, doublons.id):]
     ELSE
-        segments[:array_position(segments,doublons.id)] 
-        ||(SELECT max(id)+1 FROM public.segments)::int|| 
-        segments[array_position(segments, doublons.id)+1:] 
+        	segments[:array_position(segments,doublons.id)] 
+        	||(SELECT max(id)+1 FROM segments)::int|| 
+        	segments[array_position(segments, doublons.id)+1:] 
     END
-    FROM doublons
-    Where doublons.id = any(segments);
+    FROM 	doublons
+    WHERE 	doublons.id = ANY(segments);
 
 
 
     -- On met à jour l'id du nouveau segment
-    UPDATE public.segments
-    SET  id  = (SELECT max(id)+1 FROM public.segments) 
-    WHERE fid  = new.fid;
+    UPDATE 	segments
+    SET  	id  = (SELECT max(id)+1 FROM segments) 
+    WHERE	fid  = new.fid;
 
 
 
@@ -127,18 +129,18 @@ IF (TG_OP = 'INSERT') THEN
 	WITH
         -- On cherche les segments de compositions qui ne sont pas dans segments
 	    segments_inexistants AS (
-            SELECT segid,
-                   id 
-            FROM compositions,
-                 unnest(segments) AS segid 
-	        WHERE segid NOT IN (SELECT segments.id FROM segments)
+            SELECT 	segid,
+                   	id 
+            FROM 	compositions,
+                 	unnest(segments) AS segid 
+	        WHERE 	segid NOT IN (SELECT segments.id FROM segments)
         ) 
 
     -- On les supprime.
-	UPDATE compositions
-	SET segments = array_remove(segments, segments_inexistants.segid)
-	FROM segments_inexistants
-	WHERE compositions.id = segments_inexistants.id;
+	UPDATE 	compositions
+	SET 	segments = array_remove(segments, segments_inexistants.segid)
+	FROM 	segments_inexistants
+	WHERE 	compositions.id = segments_inexistants.id;
 
 END IF;
 
