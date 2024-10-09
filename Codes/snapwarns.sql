bc21086eb27531ad296b781aa75acfb74e28efef
UlysselaGlisse <ulysse38250@gmail.com>
2024-10-09 12:26:52 +0200

    backup

--- Codes/snapwarns.sql
+++ Codes/snapwarns.sql
@@ -5,16 +5,16 @@
 *****************       and check if two followings segments have the same id      ************************/
 
 WITH a AS (SELECT compositions.id as compoid, ordinality AS num, segid, st_collect(segments.geom) AS geom
-		FROM compositions, UNNEST(segments) WITH ordinality AS segid 
-		JOIN segments ON segments.id = segid 
+		FROM compositions, UNNEST(segments) WITH ordinality AS segid
+		JOIN segments ON segments.id = segid
 		GROUP BY num, compoid, segid)
 
-SELECT b.segid, null as num, b.geom 
+SELECT b.segid, null as num, b.geom
 FROM a join a b on a.compoid = b.compoid and a.num+1 = b.num
 WHERE st_distance(a.geom,b.geom) > 0.0000001 or a.segid = b.segid
 GROUP BY b.segid, b.geom
 
-UNION 
+UNION
 
 /********************** Check if all the segments inside compositions exists ***************/
 
@@ -22,9 +22,9 @@ SELECT segid, ordinality AS num, geom
 FROM compositions, unnest(segments) WITH ordinality AS segid
 WHERE segid NOT IN (SELECT id FROM segments)
 
-UNION 
+UNION
 
-/***************** Add random errors, just to keep the layer after Qgsi reload *************/
+/***************** Add random errors, just to keep the layer after Qgis reload *************/
 
 SELECT id , null as num, geom
 FROM compositions
