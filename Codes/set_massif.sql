64d16298b937254506fbee322d9d817010e691b5
UlysselaGlisse <ulysse38250@gmail.com>
2024-10-26 12:03:27 +0200

    backup

--- Codes/set_massif.sql
+++ Codes/set_massif.sql
@@ -1,18 +1,31 @@
-/* Il faut avoir dans la BD la couche districts de Météo France.*/
-
-WITH 
-	district as(
-	SELECT geometry 
-	FROM districts 
-	WHERE id = 6)
-
-/* id correspond au numéro du district météo france :
-1 : Chablais, 2 : Aravis, 3: Mont-Blanc, 4 : Bauges, 5: Beaufortain, 6: Tarentaise,
-7: Chartreuse, 8: Belledonne, 9: Maurienne, 10: Vanoise, 11 : Haut-Maurienne, 
-12 : Grandes Rousse, 13 : Cerces, 14: Vercors, 15 : Oisans, 16 : Écrins-Est,
-17 : Queyras, 18 : Dévoluy, 19 : Écrins-Sud, 20 : Embrunais, 21 : Ubaye, 22 */
-
-UPDATE compositions
-SET massif = 'Tarentaise'
-	FROM district 
-	WHERE St_Contains(district.geometry, compositions.geom) AND massif IS NULL
+WITH district AS (
+    SELECT id, geometry 
+    FROM districts
+)
+SELECT 
+    CASE 
+        WHEN district.id = 1 THEN 'Chablais'
+        WHEN district.id = 2 THEN 'Aravis'
+        WHEN district.id = 3 THEN 'Mont-Blanc'
+        WHEN district.id = 4 THEN 'Bauges'
+        WHEN district.id = 5 THEN 'Beaufortain'
+        WHEN district.id = 6 THEN 'Tarentaise'
+        WHEN district.id = 7 THEN 'Chartreuse'
+        WHEN district.id = 8 THEN 'Belledonne'
+        WHEN district.id = 9 THEN 'Maurienne'
+        WHEN district.id = 10 THEN 'Vanoise'
+        WHEN district.id = 11 THEN 'Haut-Maurienne'
+        WHEN district.id = 12 THEN 'Grandes Rousses'
+        WHEN district.id = 13 THEN 'Cerces'
+        WHEN district.id = 14 THEN 'Vercors'
+        WHEN district.id = 15 THEN 'Oisans'
+        WHEN district.id = 16 THEN 'Écrins-Est'
+        WHEN district.id = 17 THEN 'Queyras'
+        WHEN district.id = 18 THEN 'Dévoluy'
+        WHEN district.id = 19 THEN 'Écrins-Sud'
+        WHEN district.id = 20 THEN 'Embrunais'
+        WHEN district.id = 21 THEN 'Ubaye'
+    END AS massif
+FROM district
+JOIN segments ON ST_Contains(district.geometry, segments.geom)
+WHERE segments.id = (SELECT MAX(id) FROM segments);
