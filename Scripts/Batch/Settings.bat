b332a5500e18238791c8b97fa95020795030ce80
skitourenguru <skitourenguru@gmail.com>
2025-09-22 11:38:44 +0200

    Italy as default

--- Scripts/Batch/Settings.bat
+++ Scripts/Batch/Settings.bat
@@ -2,10 +2,10 @@ rem The working dir of the Github repository
 set GITHUB_DIR=D:\Secure\Code\Routes
 
 rem The directory, where the final network shall be exported to and where it can be processed by qGis
-set PROCESSING_DIR=D:\Secure\GPS\Projekte\Switzerland\ART\Skitourenguru\AU\segments
+set PROCESSING_DIR=D:\Secure\GPS\Projekte\Switzerland\ART\Skitourenguru\IT\segments
 
 rem Specifies the CRS (Coordinate Reference Sysstem) of the dataset
-set EPSG=31287
+set EPSG=32632
 
 rem Specifies the name of the dataset
-set NAME=Austria
+set NAME=Italy
