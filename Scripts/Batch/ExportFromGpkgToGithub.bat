1e5a3f66e0b265e29d2dc77cf8b62301dab6aab9
skitourenguru <skitourenguru@gmail.com>
2025-09-24 12:21:05 +0200

    Explicit layer name

--- Scripts/Batch/ExportFromGpkgToGithub.bat
+++ Scripts/Batch/ExportFromGpkgToGithub.bat
@@ -9,7 +9,7 @@ call "%WORKING_DIR%"\Settings.bat
 call "%WORKING_DIR%"\Environment.bat
 
 if exist "%SEGMENTS%" del /F /Q "%SEGMENTS%"
-ogr2ogr -f GeoJSON -overwrite "%SEGMENTS%" "%NETWORK%" -a_srs EPSG:%EPSG% -lco RFC7946=NO -lco COORDINATE_PRECISION=2 -sql "SELECT id AS id, * FROM Segments" --debug %DEBUG%
+ogr2ogr -f GeoJSON -overwrite "%SEGMENTS%" "%NETWORK%" -nln Segments -a_srs EPSG:%EPSG% -lco RFC7946=NO -lco COORDINATE_PRECISION=2 -sql "SELECT id AS id, * FROM Segments" --debug %DEBUG%
 
 if exist "%COMPOSITIONS%" del /F /Q "%COMPOSITIONS%"
 ogr2ogr -f GeoJSON -overwrite "%COMPOSITIONS%" "%NETWORK%" Compositions --debug %DEBUG%
