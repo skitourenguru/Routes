33d117c3f08ce845bf7fe04dd3949b48df322de7
skitourenguru <skitourenguru@gmail.com>
2025-09-24 11:00:30 +0200

    Script corrected

--- Scripts/Batch/ExportFromGpkgToGithub.bat
+++ Scripts/Batch/ExportFromGpkgToGithub.bat
@@ -8,7 +8,10 @@ set WORKING_DIR=%WORKING_DIR:~0,-1%
 call "%WORKING_DIR%"\Settings.bat
 call "%WORKING_DIR%"\Environment.bat
 
-ogr2ogr -f GeoJSON -overwrite "%SEGMENTS%" "%NETWORK%" Segments -a_srs EPSG:%EPSG% -lco RFC7946=NO -lco COORDINATE_PRECISION=2 --debug %DEBUG%
+if exist "%SEGMENTS%" del /F /Q "%SEGMENTS%"
+ogr2ogr -f GeoJSON -overwrite "%SEGMENTS%" "%NETWORK%" -a_srs EPSG:%EPSG% -lco RFC7946=NO -lco COORDINATE_PRECISION=2 -sql "SELECT id AS id, * FROM Segments" --debug %DEBUG%
+
+if exist "%COMPOSITIONS%" del /F /Q "%COMPOSITIONS%"
 ogr2ogr -f GeoJSON -overwrite "%COMPOSITIONS%" "%NETWORK%" Compositions --debug %DEBUG%
 
 pause
